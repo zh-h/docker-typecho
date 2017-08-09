68a15f1c4345d543f3fc05975780fb1fa3c84581
zohar <zh.h@outlook.com>
2017-08-09 17:07:06 +0800

    覆盖typeo文件

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-if [ ! -d "./install"]; then 
-    tar -xvf 1.0.14.10.10.-release.tar.gz
-    cp -r build/* ./
-    rm -rf build/
-fi 
+tar -xvf 1.0.14.10.10.-release.tar.gz
+cp -r build/* ./
+rm -rf build/
 chown -R www-data:www-data .
 php-fpm
